2282bc8ed346841aad0e6ab6c278852011cb439d
Irene Brignoli <ire.brignoli@gmail.com>
2023-03-03 16:07:33 +0100

    fine esercizio base - inizio bonus

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
 Bonus
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
